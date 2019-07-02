0e78a03f9c249da94ed5ec2b72eee91e69223165
Andy Chen <andycandomore@gmail.com>
2019-07-01 21:25:04 -0700

    add producer

--- java_env/src/main/java/com/github/amazingandyyy/tutorial1/ProducerDemo.java
+++ java_env/src/main/java/com/github/amazingandyyy/tutorial1/ProducerDemo.java
@@ -1,7 +1,32 @@
 package com.github.amazingandyyy.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String bootstrapServer = "localhost:9092";
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer <String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> record= new ProducerRecord<String, String>("first_topic", "heloo world");
+
+        // send data - asynchronous
+        producer.send(record);
+        // flush producer
+        producer.flush();
+        // flush and close producer
+        producer.close();
     }
 }
---
+++ java_env/src/main/java/com/github/amazingandyyy/tutorial1/ProducerDemoKeys.java
@@ -0,0 +1,60 @@
+package com.github.amazingandyyy.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoKeys {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+        String bootstrapServer = "localhost:9092";
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer <String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i=0; i<10; i++) {
+
+            String topic = "first_topic";
+            String value = "hello world " + Integer.toString(i);
+            String key = "Key_" + Integer.toString(i);
+
+            // create a producer record
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>(topic, key, value);
+
+            logger.info("Key: " + key); // log the key
+
+            // send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // execute every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        // the record was successfully sent
+                        logger.info("Receive new metadata."
+                                + "\n" + "Topic: " + recordMetadata.topic()
+                                + "\n" + "Partition: " + recordMetadata.partition()
+                                + "\n" + "Offset: " + recordMetadata.offset()
+                                + "\n" + "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get(); // block the .send() to make it synchronous
+        }
+        // flush producer
+        producer.flush();
+        // flush and close producer
+        producer.close();
+    }
+}
